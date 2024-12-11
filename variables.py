c0616087d2c2add1f5ae6db4feb611d2ea4e2098
Danny Aguayo <dannyaguayo30@gmail.com>
2024-12-11 18:17:53 -0500

    first commit

---
+++ variables.py
@@ -0,0 +1,16 @@
+saludo = "hola mundo"
+
+numero = 10
+numero += 5 
+numero -= 2
+
+print(type (numero))
+print(type (saludo))
+print(type(numero), type (saludo))
+
+
+peso = 65.5 
+print(type(peso))
+
+mayorEdad = True 
+print(type(mayorEdad))
